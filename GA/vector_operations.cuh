bd5fe06a55b1ca27770d7ca216a432cfdf854d83
Adam Dziomdziora <190140@edu.p.lodz.pl>
2020-11-21 12:57:36 +0200

    added vector oprators and vector operations

---
+++ GA/vector_operations.cuh
@@ -0,0 +1,50 @@
+#include<vector>
+#include <algorithm>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+void MultiplyVectorByScalar(vector<float>& v, float k) {
+	transform(v.begin(), v.end(), v.begin(), [k](float& c) { return c * k; });
+}
+
+void Multiply2DVectorByScalar(vector<vector<float>>& v, float k) {
+	std::for_each(v.begin(), v.end(),
+		[&k](std::vector<float>& v) {
+			MultiplyVectorByScalar(v, k);
+		}
+	);
+}
+
+template <typename T>
+void Print2DVector(vector<vector<T>>& v, string vectorName)
+{
+	cout << "\n" << "=============== " << vectorName << " ===============\n";
+	for (int i = 0; i < v.size(); i++) {
+		vector<T> tmp = v[i];
+		for (int j = 0; j < tmp.size(); j++) {
+			cout << tmp[j] << " ";
+		}
+		cout << "\n";
+	}
+	cout << "==========================================\n";
+}
+
+template <typename T>
+void Print3DVector(vector<vector<vector<T>>>& v, string vectorName)
+{
+	cout << "\n" << "=============== " << vectorName << " ===============\n";
+	for (int i = 0; i < v.size(); i++) {
+		vector<vector<T>> tmp2D = v[i];
+		for (int j = 0; j < tmp2D.size(); j++) {
+			vector<T> tmp = tmp2D[j];
+			for (int k = 0; k < tmp.size(); k++) {
+				cout << tmp[k] << " ";
+			}
+			cout << "\n";
+		}
+		cout << "\n\n\n";
+	}
+	cout << "==========================================\n";
+}
---
+++ GA/vector_operators.cuh
@@ -0,0 +1,136 @@
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+template <typename T>
+vector<T> operator*(const vector<T>& a, const vector<T>& b)
+{
+	if (a.size() != b.size()) {
+		throw "Vectors must be equal size";
+	}
+
+	vector<T> result;
+	result.reserve(a.size());
+
+	transform(a.begin(), a.end(), b.begin(),
+		back_inserter(result), multiplies<T>());
+	return result;
+}
+
+template <typename T>
+vector<T> operator/(const vector<T>& a, const vector<T>& b)
+{
+	if (a.size() != b.size()) {
+		throw "Vectors must be equal size";
+	}
+
+	vector<T> result;
+	result.reserve(a.size());
+
+	transform(a.begin(), a.end(), b.begin(),
+		back_inserter(result), divides<T>());
+	return result;
+}
+
+template <typename T>
+vector<T> operator+(const vector<T>& a, const vector<T>& b)
+{
+	if (a.size() != b.size()) {
+		throw "Vectors must be equal size";
+	}
+
+	vector<T> result;
+	result.reserve(a.size());
+
+	transform(a.begin(), a.end(), b.begin(),
+		back_inserter(result), plus<T>());
+	return result;
+}
+
+template <typename T>
+vector<T> operator-(const vector<T>& a, const vector<T>& b)
+{
+	if (a.size() != b.size()) {
+		throw "Vectors must be equal size";
+	}
+
+	vector<T> result;
+	result.reserve(a.size());
+
+	transform(a.begin(), a.end(), b.begin(),
+		back_inserter(result), minus<T>());
+	return result;
+}
+
+template <typename T>
+vector<vector<T>> operator+(const vector<vector<T>>& a, const vector<vector<T>>& b)
+{
+	if (a.size() != b.size()) {
+		throw "Vectors must be equal size";
+	}
+
+	vector<vector<T>> result;
+	result.reserve(a.size());
+
+	for (int i = 0; i < a.size(); i++) {
+		auto tmp = a[i] + b[i];
+		result.push_back(tmp);
+	}
+
+	return result;
+}
+
+template <typename T>
+vector<vector<T>> operator-(const vector<vector<T>>& a, const vector<vector<T>>& b)
+{
+	if (a.size() != b.size()) {
+		throw "Vectors must be equal size";
+	}
+
+	vector<vector<T>> result;
+	result.reserve(a.size());
+
+	for (int i = 0; i < a.size(); i++) {
+		auto tmp = a[i] - b[i];
+		result.push_back(tmp);
+	}
+
+	return result;
+}
+
+template <typename T>
+vector<vector<T>> operator*(const vector<vector<T>>& a, const vector<vector<T>>& b)
+{
+	if (a.size() != b.size()) {
+		throw "Vectors must be equal size";
+	}
+
+	vector<vector<T>> result;
+	result.reserve(a.size());
+
+	for (int i = 0; i < a.size(); i++) {
+		auto tmp = a[i] * b[i];
+		result.push_back(tmp);
+	}
+
+	return result;
+}
+
+template <typename T>
+vector<vector<T>> operator/(const vector<vector<T>>& a, const vector<vector<T>>& b)
+{
+	if (a.size() != b.size()) {
+		throw "Vectors must be equal size";
+	}
+
+	vector<vector<T>> result;
+	result.reserve(a.size());
+
+	for (int i = 0; i < a.size(); i++) {
+		auto tmp = a[i] / b[i];
+		result.push_back(tmp);
+	}
+
+	return result;
+}
